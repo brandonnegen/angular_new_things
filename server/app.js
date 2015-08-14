e8b531d14cfdb1b164a0e105d7c07d4ab3d3f23d
brandonnegen <bnegen@gmail.com>
2015-08-14 13:47:58 -0500

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port', (process.env.PORT || 8000));
+
+app.get("/*", function(req, res, next){
+
+    var file = req.params[0] || '/assets/views/index.html';
+    res.sendFile(path.join(__dirname, "./public/", file));
+});
+
+app.listen(app.get('port'), function(){
+   console.log("Listening on port: " + app.get('port'));
+});
+
+module.exports = app;
